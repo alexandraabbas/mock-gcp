89d23d66e153936d39471e794eaba27ca86f7f32
Alexandra <alexandra.abbas@datatonic.com>
2019-11-08 13:19:46 +0000

    Generate project id

--- mockgcp/backend.py
+++ mockgcp/backend.py
@@ -1,5 +1,11 @@
+import uuid
+
+
 class StorageBackend:
-    def __init__(self):
+    def __init__(self, project=None):
+        if project is None:
+            project = "test-project-" + str(uuid.uuid1())
+        self.project = project
         self.buckets = {}
         self.blobs = {}
 
