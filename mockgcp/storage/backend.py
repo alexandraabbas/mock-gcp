1ce1e9334650555c88fc8c98605852fa80c6d186
Alexandra <alexandra.abbas@datatonic.com>
2019-11-10 15:53:07 +0000

    Create reset and decorator method

--- mockgcp/storage/backend.py
+++ mockgcp/storage/backend.py
@@ -1,7 +1,11 @@
 import uuid
+import functools
+from unittest.mock import patch
+
+from mockgcp.storage.client import Client
 
 
-class StorageBackend:
+class StorageBackend(object):
     def __init__(self, project=None):
         if project is None:
             project = "test-project-" + str(uuid.uuid1())
@@ -9,5 +13,20 @@ class StorageBackend:
         self.buckets = {}
         self.blobs = {}
 
+    def reset(self):
+        self.buckets = {}
+        self.blobs = {}
+
+    def mock_storage(self, func):
+        def wrapper(*args, **kwargs):
+            self.reset()
+            with patch("google.cloud.storage.Client", Client):
+                return func(*args, **kwargs)
+
+        functools.update_wrapper(wrapper, func)
+        wrapper.__wrapped__ = func
+        return wrapper
+
 
 backend = StorageBackend()
+mock_storage = backend.mock_storage
