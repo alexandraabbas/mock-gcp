1163239b1861085d89350fbbd6d802090cef83d6
Alexandra <alexandra.abbas@datatonic.com>
2019-11-10 15:52:42 +0000

    Remove initialize_backend

--- tests/test_storage/test_client.py
+++ tests/test_storage/test_client.py
@@ -1,8 +1,8 @@
-from mockgcp.storage.decorators import mock_storage
+from mockgcp.storage.backend import mock_storage
 from mockgcp.storage.client import Client
-from google.cloud import storage
 from mockgcp.storage.bucket import Bucket
 
+from google.cloud import storage
 from google.cloud.exceptions import NotFound
 
 import pytest
@@ -11,7 +11,7 @@ import sys
 
 @mock_storage
 def test_bucket_constructor():
-    client = storage.Client(initialize_backend=True)
+    client = storage.Client()
     assert isinstance(client, Client)
 
     bucket = client.bucket("test-bucket-name-v1")
@@ -23,13 +23,13 @@ def test_bucket_constructor():
 @mock_storage
 def test_bucket_constructor_with_invalid_name():
     with pytest.raises(ValueError):
-        client = storage.Client(initialize_backend=True)
+        client = storage.Client()
         bucket = client.bucket("test-bucket-name-")
 
 
 @mock_storage
 def test_get_bucket_with_existing_bucket_name():
-    client = storage.Client(initialize_backend=True)
+    client = storage.Client()
     bucket = client.create_bucket("test-bucket-name-v2")
 
     assert client.get_bucket("test-bucket-name-v2") is bucket
@@ -38,7 +38,7 @@ def test_get_bucket_with_existing_bucket_name():
 @mock_storage
 def test_get_bucket_with_non_existing_bucket_name():
     with pytest.raises(NotFound):
-        client = storage.Client(initialize_backend=True)
+        client = storage.Client()
         buckets = list(client.list_buckets())
         print([bucket.name for bucket in buckets])
         client.get_bucket("test-bucket-name-v2")
